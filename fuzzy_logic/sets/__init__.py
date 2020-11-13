11f782e150dc69ced93ca0dbb1ef3612e57d8a75
cbermudez <cbermudez.dev@gmail.com>
2020-11-13 14:58:30 -0500

    Add function to graphic fuzz sets

---
+++ fuzzy_logic/sets/__init__.py
@@ -0,0 +1,1 @@
+from .fuzzy_set import FSet
--- fuzzy_logic/sets/fuzzy_set.py
+++ fuzzy_logic/sets/fuzzy_set.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 
 UMAX = max # Union method using max
 USUM = lambda x, y: min(x + y, 1) # Union method using limited sum
@@ -8,7 +10,8 @@ ISUB = lambda x, y: max(x + y - 1, 0) # Intersection method using limited substr
 IPRO = lambda x, y: x * y # Intersection method using product
 
 class FSet:
-    def __init__(self, membership:dict, union_method=UMAX, intersec_method=IMIN):
+    def __init__(self, name:str,membership:dict, union_method=UMAX, intersec_method=IMIN):
+        self.name = name
         self.membership = membership
         self.umethod = union_method
         self.imethod = intersec_method
@@ -19,7 +22,7 @@ class FSet:
             if not x in other.membership.keys():
                 raise ValueError('Domains sets must be identical.')
             union_memb[x] = self.umethod(self.membership[x], other.membership[x])
-        return FSet(union_memb, union_method=self.umethod, intersec_method=self.imethod)
+        return FSet(f'union_({self.name})_({other.name})', union_memb, union_method=self.umethod, intersec_method=self.imethod)
     
     def __mul__(self, other):
         int_memb = dict()
@@ -27,4 +30,22 @@ class FSet:
             if not x in other.membership.keys():
                 raise ValueError('Domains sets must be identical.')
             int_memb[x] = self.imethod(self.membership[x], other.membership[x])
-        return FSet(int_memb, union_method=self.umethod, intersec_method=self.imethod)
+        return FSet(f'inter_({self.name})_({other.name})', int_memb, union_method=self.umethod, intersec_method=self.imethod)
+
+    def graph_data(self):
+        data = list(self.membership.items())
+        data.sort()
+        x_data = []
+        y_data = []
+        for x, y in data:
+            x_data.append(x)
+            y_data.append(y)
+        return x_data, y_data
+
+    def graph(self):
+        x_data, y_data = self.graph_data()
+        plt.title(self.name)
+        plt.xlabel('Domain values')
+        plt.ylabel('Membership values')
+        plt.plot(x_data, y_data)
+        plt.show()
