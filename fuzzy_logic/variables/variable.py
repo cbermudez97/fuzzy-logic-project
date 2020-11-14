8f57fe5e98ad8a6ea633b9f3f5bde77042044335
cbermudez <cbermudez.dev@gmail.com>
2020-11-14 12:51:32 -0500

    Implement method to graph variables

--- fuzzy_logic/variables/variable.py
+++ fuzzy_logic/variables/variable.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from ..sets import FSet
 
 
@@ -11,3 +13,12 @@ class Variable:
 
     def fuzzify(self, x, dname):
         return self.descriptors[dname].membership(x)
+
+    def graph(self, end_des=5, step=0.05):
+        plots = []
+        for it, desc in enumerate(self.descriptors.values()):
+            x_data = desc.get_domain_sample(end_des=end_des, step=step)
+            y_data = [ desc.membership(x) for x in x_data ]
+            plots.append(plt.plot(x_data, y_data, f'C{it+1}', label=desc.name))
+        plt.legend()
+        plt.show()
