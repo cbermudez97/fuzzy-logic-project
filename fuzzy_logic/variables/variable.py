03f8c8ae0fe470ffd7f44ea9f525a8f22bbc4e4e
cbermudez <cbermudez.dev@gmail.com>
2020-11-14 11:43:54 -0500

    Implement basic linguistic variable class

---
+++ fuzzy_logic/variables/variable.py
@@ -0,0 +1,15 @@
+import re
+
+from ..sets import FSet
+
+
+class Variable:
+    def __init__(self, name, *args):
+        assert all([ isinstance(fset, FSet) for fset in args ]), 'All sets should be valid ones.'
+        self.name = name
+        self.descriptors = dict()
+        for fset in args:
+            self.descriptors[fset.name] = fset
+
+    def fuzzify(self, x, dname):
+        return self.descriptors[dname].membership(x)
